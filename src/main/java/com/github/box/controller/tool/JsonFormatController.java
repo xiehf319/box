3d8496b0514cc0a2aeb47cbdc7c91e85fba70e11
xhf319 <370693739@qq.com>
2020-03-09 23:57:22 +0800

    调整填充效果

--- src/main/java/com/github/box/controller/tool/JsonFormatController.java
+++ src/main/java/com/github/box/controller/tool/JsonFormatController.java
@@ -11,6 +11,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
 import org.springframework.util.StringUtils;
 
 import java.net.URL;
@@ -18,72 +19,67 @@ import java.util.ResourceBundle;
 
 @FXMLController
 public class JsonFormatController implements Initializable {
-    @FXML
-    private TextArea rawJson;
-
-    @FXML
-    private TreeTableView<String> treeTableView;
-
-    @FXML
-    private TreeTableColumn<String, Object> treeTableKey;
-
-    @FXML
-    private TreeTableColumn<String, Object> treeTableValue;
-
-    @FXML
-    private Button format;
-
-    @FXML
-    private Button beauty;
-
-    @FXML
-    private Label errMsg;
+//    @FXML
+//    private TextArea rawJson;
+//
+//    @FXML
+//    private Button format;
+//
+//    @FXML
+//    private Button beauty;
+//
+//    @FXML
+//    private Label errMsg;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        adapt();
     }
 
-    @FXML
-    private void beauty(ActionEvent event) {
-        errMsg.setText("");
-        String s = rawJson.textProperty().get();
-        if (StringUtils.isEmpty(s)) {
-            errMsg.setText("json 不能为空");
-            return;
-        }
-        String result = validJson(s);
-        if (result != null) {
-            rawJson.textProperty().set(result);
-        }
-    }
+//    @FXML
+//    private void beauty(ActionEvent event) {
+//        errMsg.setText("");
+//        String s = rawJson.textProperty().get();
+//        if (StringUtils.isEmpty(s)) {
+//            errMsg.setText("json 不能为空");
+//            return;
+//        }
+//        String result = validJson(s);
+//        if (result != null) {
+//            rawJson.textProperty().set(result);
+//        }
+//    }
 
-    private String validJson(String s) {
-        JSONValidator validator = JSONValidator.from(s);
-        try {
-            boolean valid = validator.validate();
-            if (!valid) {
-                errMsg.setText("json格式错误");
-                return null;
-            }
-            JSONObject jsonObject = JSONObject.parseObject(s);
-            String result = JSON.toJSONString(jsonObject, SerializeConfig.globalInstance, SerializerFeature.PrettyFormat);
-        } catch (Exception e) {
-            errMsg.setText("json格式发生错误");
-        }
-        return null;
-    }
+//    private String validJson(String s) {
+//        JSONValidator validator = JSONValidator.from(s);
+//        try {
+//            boolean valid = validator.validate();
+//            if (!valid) {
+//                errMsg.setText("json格式错误");
+//                return null;
+//            }
+//            JSONObject jsonObject = JSONObject.parseObject(s);
+//            String result = JSON.toJSONString(jsonObject, SerializeConfig.globalInstance, SerializerFeature.PrettyFormat);
+//        } catch (Exception e) {
+//            errMsg.setText("json格式发生错误");
+//        }
+//        return null;
+//    }
+//
+//    @FXML
+//    private void format(ActionEvent event) {
+//        String s = rawJson.textProperty().get();
+//        if (StringUtils.isEmpty(s)) {
+//            errMsg.setText("json 不能为空");
+//            return;
+//        }
+//        String result = validJson(s);
+//        if (result != null) {
+//            JSONObject jsonObject = JSONObject.parseObject(result);
+//        }
+//    }
+//
+    private void adapt() {
 
-    @FXML
-    private void format(ActionEvent event) {
-        String s = rawJson.textProperty().get();
-        if (StringUtils.isEmpty(s)) {
-            errMsg.setText("json 不能为空");
-            return;
-        }
-        String result = validJson(s);
-        if (result != null) {
-            JSONObject jsonObject = JSONObject.parseObject(result);
-        }
     }
 }
